fac02f409d070082e2c4fcc6894d3195706bb024
SilvaL011 <leandross2006@gmail.com>
2025-11-23 13:01:36 -0500

    Saving progress

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routers.ingredients_router import router as ingredients_router
 from routers.plate_router import router as plate_router
 from routers.impact_router import router as impact_router
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 app.include_router(ingredients_router)
 app.include_router(plate_router)
 app.include_router(impact_router)
